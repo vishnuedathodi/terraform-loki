d3dfa3934440927cc7b48904fe44a898cd9d0147
vishnuedathodi <154342576+vishnuedathodi@users.noreply.github.com>
2024-03-22 10:59:06 +0530

    Add files via upload

---
+++ helm-loki.tf
@@ -0,0 +1,58 @@
+
+resource "time_sleep" "wait_for_kubernetes" {
+
+    depends_on = [
+        data.aws_eks_cluster.tg-tekton-eks-cluster
+    ]
+
+    create_duration = "20s"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+   command    = "cat ~/.kube/config"
+  }
+}
+
+resource "helm_release" "loki" {
+ 
+  name       = "loki-v1"  
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-stack"
+  namespace  = "prometheus"
+  create_namespace = false
+  version    = "2.7.0"
+  timeout = 2000
+  
+
+# set {
+#     name  = "podSecurityPolicy.enabled"
+#     value = true
+#   }
+  set {
+    name  = "fluent-bit.enabled"
+    value = false
+  }
+  set {
+    name  = "promtail.enabled"
+    value = true
+  }
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
+  set {
+    name = "server\\.resources"
+    value = yamlencode({
+      limits = {
+        cpu    = "200m"
+        memory = "50Mi"
+      }
+      requests = {
+        cpu    = "100m"
+        memory = "30Mi"
+      }
+    })
+  }
+}
+  
